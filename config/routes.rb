a00c2e0ff7d0f5c5ca3e7f17f6789c1964334c6f
Alex <alexcannon2@live.com>
2017-04-25 03:00:54 +0000

    create views and config route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+
+  Rails.application.routes.draw do
+  root to: "posts#index"
+
+  resources :posts
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
