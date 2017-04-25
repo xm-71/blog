d3be082cedbbb2c6fd60920b6528f3d3724db3a4
Alex <alexcannon2@live.com>
2017-04-25 03:40:42 +0000

    testing to see if will push to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-
-
-
-  mount Ckeditor::Engine => '/ckeditor'
   root to: "posts#index"
 
   resources :posts
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
